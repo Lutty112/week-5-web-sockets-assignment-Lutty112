4ac3a5d4eca49946685953713a2be31a2be7cfe5
Lutty112 <lutfikhatib112@gmail.com>
2025-07-17 17:01:12 +0300

    Modified server.js file

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
 const connectDB = require("./config/db");
-const cors = require('cors')
+
 
 // Load environment variables
 dotenv.config();
